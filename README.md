56560bc78643e50ee5769b119db486bfedfe77f7
Stratos Pavlakis <pavlakis.stratos@gmail.com>
2018-10-15 23:50:47 +0300

    Set redis connection via ENV

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 > A pet Node project for experimenting with deployment strategies
 
 ### Setup
-```bash
-npm i
-```
+
+1. Install and run Redis
+2. Perform an `npm i`
 
 ### Run
 ```
@@ -16,11 +16,14 @@ PORT=8080 npm run server
 
 # Background Jobs
 npm run jobs
+
+# Background Jobs with custom Redis URL (defaults to redis://127.0.0.1:6379)
+REDIS_URL="redis://<host>:<port>" npm run jobs
 ```
 
 ### Healthcheck
 
 ```
-curl -f http://localhost:3000/health #responds 200 - OK 
+curl -f http://localhost:3000/health #responds 200 - OK
 ```
 
--- bin/jobs.js
+++ bin/jobs.js
@@ -1,9 +1,11 @@
 const Queue = require("bull");
 const path = require("path");
 
-const job1Queue = new Queue("job1");
-const job2Queue = new Queue("job2");
-const job3Queue = new Queue("job3");
+const redis = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
+
+const job1Queue = new Queue("job1", redis);
+const job2Queue = new Queue("job2", redis);
+const job3Queue = new Queue("job3", redis);
 
 const payload = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
