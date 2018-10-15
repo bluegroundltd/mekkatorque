78d4eb4e96307ca083e39d3734edbbbad6891e57
Stratos Pavlakis <pavlakis.stratos@gmail.com>
2018-10-15 16:20:12 +0300

    Add /health endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res) => {
-  res.render('index', { title: 'Express' });
+router.get("/", (req, res) => {
+  res.render("index", { title: "Hello world!" });
+});
+
+router.get("/health", (req, res) => {
+  res.status(200).send("OK");
 });
 
 module.exports = router;
